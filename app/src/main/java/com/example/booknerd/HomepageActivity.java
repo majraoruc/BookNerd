34e87a858fd34f7e14e0a5a359afcffae485d6b5
majraoruc <majra.oruc@stu.ibu.edu.ba>
2020-06-01 22:23:03 +0200

    register + login with firebase

---
+++ app/src/main/java/com/example/booknerd/HomepageActivity.java
@@ -0,0 +1,16 @@
+package com.example.booknerd;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
---
+++ app/src/main/java/com/example/booknerd/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.example.booknerd;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+EditText useremail, userpassword;
+Button loginbutton;
+FirebaseAuth fAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        useremail=findViewById(R.id.username_field);
+        userpassword=findViewById(R.id.password_field);
+        loginbutton=findViewById(R.id.login_button);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=useremail.getText().toString().trim();
+                String password=userpassword.getText().toString().trim();
+                if(TextUtils.isEmpty(email)){
+                    useremail.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    userpassword.setError("Password is required");
+                    return;
+                }
+                if(password.length()<6){
+                    userpassword.setError("Password bust be longer than 5 characters");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"Login successful", Toast.LENGTH_SHORT).show();
+                            //startActivity(new Intent(getApplicationContext(), HomepageActivity.class));
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Error! "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
